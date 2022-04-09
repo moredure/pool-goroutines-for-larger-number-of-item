d962ff2080753e331d05d3b71d1082022a635bd6
Mikhail Faraponov <11322032+moredure@users.noreply.github.com>
2022-04-09 13:28:58 +0300

    Update heap.go

--- heap.go
+++ heap.go
@@ -28,21 +28,3 @@ func (s *SyncMutexItemsHeap) Pop() *Account {
 	}
 	return heap.Pop(&s.Items).(*Account)
 }
-
-type Accounts map[string]*Account
-
-func (accounts Accounts) AddAccount(id string) {
-	// grab from db
-	a := &Account{
-		stopped:        make(chan struct{}), // atomic
-		dataArrived:    false,               // for epoll? to check non blocking whether the date arrived
-		state:          0,
-		nextAccessTime: time.Now(),
-	}
-	accounts[id] = a
-	acheap.Push(a)
-}
-
-func (accounts Accounts) onStopAccount(id string) {
-	accounts[id].Stop()
-}
